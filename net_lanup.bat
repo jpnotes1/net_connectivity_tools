c7de9ebcfd7204bde6127f267dd4782bca23760c
jpnotes1 <j_piscapo@yahoo.com>
2026-02-18 09:55:33 +0000

    Revise copyright and add usage disclaimer
    Updated copyright notice and added a disclaimer about usage risks.

--- net_lanup.bat
+++ net_lanup.bat
@@ -7,9 +7,11 @@ REM ------------------------------
 REM MIT License Info
 REM ------------------------------
 :: Lan Check
-:: Copyright (c) 2026 Steven Lizama / Authorship Studios
+:: Copyright (c) 2026 Steven Lizama / Authorship Studios  All rights reserved.
 :: Released under MIT License
+REM Use at your own risk. The author is not responsible for any system issues or data loss.
 
 @echo off
 powershell -Command "$gw=(Get-NetIPConfiguration).IPv4DefaultGateway.NextHop; if (Test-Connection $gw -Quiet) { Write-Host 'LAN is up' } else { Write-Host 'LAN is down' }"
 pause
+
