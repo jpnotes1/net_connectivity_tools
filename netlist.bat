96de6e8c7f8a4b095e93447c120969a1389ea808
jpnotes1 <j_piscapo@yahoo.com>
2026-02-18 10:03:51 +0000

    Revise copyright and add usage disclaimer
    Updated copyright notice and added a disclaimer.

--- netlist.bat
+++ netlist.bat
@@ -2,15 +2,13 @@ echo ===== Net_Connectivity_Tools_list =====
 echo.
 @echo off
 
-REM ------------------------------
-REM MIT License Info
-REM ------------------------------
 :: Net_Connectivity_Tools_List
-:: Copyright (c) 2026 Steven Lizama / Authorship Studios
+:: Copyright (c) 2026 Steven Lizama / Authorship Studios All rights reserved.
 :: Released under MIT License
-
+:: Use at your own risk. The author is not responsible for any system issues or data loss.
 
 REM netlist - list all files in current folder
 echo Listing files in %CD%
 dir net* /b
+
 
